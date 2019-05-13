fb96d3088c2680ea4acc41a09c45fd055d3f3a4e
JulianBarragan <julidj505@gmail.com>
2019-05-13 10:34:11 -0500

    Iniciar subida de archivos

---
+++ db/test.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE testapi;
+USE testapi;
+
+CREATE TABLE IF NOT EXISTS users (
+    `id` INT(10) unsigned NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+    `email` VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+    `password` VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `update_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `users_email_unique` (`email`)
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
