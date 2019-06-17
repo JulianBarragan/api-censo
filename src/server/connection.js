446b52245cd6bedfd003e5f7719adbbb8cf4c6af
Julian <julidj505@gmail.com>
2019-06-17 11:20:02 -0500

    Config to database

--- src/server/connection.js
+++ src/server/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  // host: 'http://192.168.0.17',
-  user: 'root',
-  password: '',
-  database: 'censo'
+  // host: 'localhost',
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'bb733aaf7c9316',
+  password: 'e28c0c28',
+  database: 'heroku_fddad1e74a608e2'
 })
 
 module.exports = connection
