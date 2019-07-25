42d3905851f0d2a2443d77339f729f01a3fcd4a4
Julian <julidj505@gmail.com>
2019-07-25 11:59:01 -0500

    producto agricola

--- src/models/rutasBasicas.js
+++ src/models/rutasBasicas.js
@@ -53,6 +53,66 @@ rutasBasicas.getEstudios = (callback) => {
   }
 }
 
+rutasBasicas.postEstudios = (userData, callback) => {
+  if (connection) {
+    connection.query('INSERT INTO grado_estudios SET ?', userData,
+      (err, result) => {
+        if (err) {
+          throw err
+        } else {
+          callback(null, {
+            'insertId': result.insertId
+          })
+        }
+      }
+    )
+  }
+}
+
+rutasBasicas.updateEstudios = (userData, callback) => {
+  if (connection) {
+    const sql = `
+      UPDATE grado_estudios SET 
+      estudios = ${connection.escape(userData.estudios)}
+      WHERE id_grado_estudios = ${connection.escape(userData.id_grado_estudios)}
+    `
+    connection.query(sql, (err, result) => {
+      if (err) {
+        throw err
+      } else {
+        callback(null, {
+          'msg': `Grado de Estudio Actualizado`
+        })
+      }
+    })
+  }
+}
+
+rutasBasicas.deleteEstudio = (id, callback) => {
+  if (connection) {
+    let sqlExists =
+      `SELECT * FROM grado_estudios WHERE id_grado_estudios = ${connection.escape(id)}`
+    connection.query(sqlExists, (_err, row) => {
+      if (row) {
+        let sql = `UPDATE grado_estudios SET estatus = 0 WHERE id_grado_estudios = ${connection.escape(id)}`
+        connection.query(sql, (err, _result) => {
+          if (err) {
+            throw err
+          } else {
+            callback(null, {
+              'msg': 'Deleted'
+            })
+          }
+        })
+      } else {
+        callback(null, {
+          'msg': 'not Exists'
+        })
+      }
+    })
+  }
+}
+
 // ============================
 //  Servicios Basicos
 // ============================
@@ -107,20 +167,40 @@ rutasBasicas.updateColonias = (userData, callback) => {
   if (connection) {
     const sql = `
       UPDATE colonias SET 
-      nombre = ${connection.escape(userData.nombre)},
-      a_paterno = ${connection.escape(userData.a_paterno)},
-      a_materno = ${connection.escape(userData.a_materno)},
-      roll = ${connection.escape(userData.roll)},
-      contrasena = ${connection.escape(userData.contrasena)},
-      telefono = ${connection.escape(userData.telefono)} 
-      WHERE id_usuario = ${connection.escape(userData.id_usuario)}
+      colonia = ${connection.escape(userData.colonia)}
+      WHERE id_colonia = ${connection.escape(userData.id_colonia)}
     `
     connection.query(sql, (err, result) => {
       if (err) {
         throw err
       } else {
         callback(null, {
-          'msg': `Usuario Actualizado`
+          'msg': `Colonia Actualizada`
+        })
+      }
+    })
+  }
+}
+
+rutasBasicas.deleteColonia = (id, callback) => {
+  if (connection) {
+    let sqlExists =
+      `SELECT * FROM colonias WHERE id_colonia = ${connection.escape(id)}`
+    connection.query(sqlExists, (_err, row) => {
+      if (row) {
+        let sql = `UPDATE colonias SET estatus = 0 WHERE id_colonia = ${connection.escape(id)}`
+        connection.query(sql, (err, _result) => {
+          if (err) {
+            throw err
+          } else {
+            callback(null, {
+              'msg': 'Deleted'
+            })
+          }
+        })
+      } else {
+        callback(null, {
+          'msg': 'not Exists'
         })
       }
     })
@@ -209,6 +289,88 @@ rutasBasicas.getServiciosImportancia = (userData, callback) => {
         }
       }
     )
+  }
+}
+
+// ============================
+//  Productos Agricolas
+// ============================
+rutasBasicas.getProductosAgricolas = (userData, callback) => {
+  if (connection) {
+    connection.query('SELECT * FROM productos_agricolas ORDER BY id_producto', userData,
+      (err, rows) => {
+        if (err) {
+          throw err
+        } else {
+          callback(null, rows)
+        }
+      }
+    )
+  }
+}
+
+rutasBasicas.postProductosAgricolas = (userData, callback) => {
+  if (connection) {
+    connection.query('INSERT INTO productos_agricolas SET ?', userData,
+      (err, result) => {
+        if (err) {
+          throw err
+        } else {
+          callback(null, {
+            'insertId': result.insertId
+          })
+        }
+      }
+    )
+  }
+}
+
+rutasBasicas.updateProductosAgricolas = (userData, callback) => {
+  if (connection) {
+    const sql = `
+      UPDATE productos_agricolas SET 
+      producto = ${connection.escape(userData.producto)},
+      tipo_riego = ${connection.escape(userData.tipo_riego)},
+      tipo_terreno = ${connection.escape(userData.tipo_terreno)},
+      id_familia = ${connection.escape(userData.id_familia)},
+      cantidad_hectareas = ${connection.escape(userData.cantidad_hectareas)},
+      producto = ${connection.escape(userData.producto)}
+      WHERE id_producto = ${connection.escape(userData.id_producto)}
+    `
+    connection.query(sql, (err, result) => {
+      if (err) {
+        throw err
+      } else {
+        callback(null, {
+          'msg': `Producto Agricola Actualizado`
+        })
+      }
+    })
+  }
+}
+
+rutasBasicas.deleteProductoAgricola = (id, callback) => {
+  if (connection) {
+    let sqlExists =
+      `SELECT * FROM productos_agricolas WHERE id_producto = ${connection.escape(id)}`
+    connection.query(sqlExists, (_err, row) => {
+      if (row) {
+        let sql = `UPDATE productos_agricolas SET estatus = 0 WHERE id_producto = ${connection.escape(id)}`
+        connection.query(sql, (err, _result) => {
+          if (err) {
+            throw err
+          } else {
+            callback(null, {
+              'msg': 'Deleted'
+            })
+          }
+        })
+      } else {
+        callback(null, {
+          'msg': 'not Exists'
+        })
+      }
+    })
   }
 }
 
--- src/routes/rutasBasicasRoutes.js
+++ src/routes/rutasBasicasRoutes.js
@@ -28,8 +28,59 @@ module.exports = app => {
     })
   })
 
-  
+  app.post('/rutas/estudios', (req, res) => {
+    const userData = {
+      id_grado_estudios: null,
+      estudios: req.body.estudios
+    }
+    Rutas.postEstudios(userData, (_err, data) => {
+      if (data && data.insertId) {
+        console.log(data)
+        res.json({
+          success: true,
+          msg: 'Grado de Estudios Insertado',
+          data: data
+        })
+      } else {
+        res.status(500).json({
+          success: false,
+          msg: 'error'
+        })
+      }
+    })
+  })
 
+  app.put('/rutas/colonias/:id_colonia', (req, res) => {
+    const userData = {
+      id_grado_estudios: req.params.id_grado_estudios,
+      estudios: req.body.estudios
+    }
+    Rutas.updateEstudios(userData, (_err, data) => {
+      if (data && data.msg) {
+        res.json({ data })
+      } else {
+        res.status(500).json({
+          success: false,
+          msg: 'Error'
+        })
+      }
+    })
+  })
+
+  app.delete('/rutas/estudios/:id', (req, res) => {
+    Rutas.deleteEstudio(req.params.id, (_err, data) => {
+      if ((data && data.msg) === 'Deleted' || data.msg === 'not Exists') {
+        res.json({
+          success: 'true',
+          data
+        })
+      } else {
+        res.status(500).json({
+          msg: 'Error'
+        })
+      }
+    })
+  })
   // ============================
   //  Servicios Basicos
   // ============================
@@ -65,6 +116,38 @@ module.exports = app => {
         res.status(500).json({
           success: false,
           msg: 'error'
+        })
+      }
+    })
+  })
+
+  app.put('/rutas/colonias/:id_colonia', (req, res) => {
+    const userData = {
+      id_colonia: req.params.id_colonia,
+      colonia: req.body.colonia
+    }
+    Rutas.updateColonias(userData, (_err, data) => {
+      if (data && data.msg) {
+        res.json({ data })
+      } else {
+        res.status(500).json({
+          success: false,
+          msg: 'Error'
+        })
+      }
+    })
+  })
+
+  app.delete('/rutas/colonias/:id', (req, res) => {
+    Rutas.deleteColonia(req.params.id, (_err, data) => {
+      if ((data && data.msg) === 'Deleted' || data.msg === 'not Exists') {
+        res.json({
+          success: 'true',
+          data
+        })
+      } else {
+        res.status(500).json({
+          msg: 'Error'
         })
       }
     })
@@ -112,6 +195,77 @@ module.exports = app => {
   app.get('/rutas/servicios_importancia', (_req, res) => {
     Rutas.getProblemasComunidad((_err, data) => {
       res.status(200).json(data)
+    })
+  })
+
+  // ============================
+  //  Productos Agricolas
+  // ============================
+  app.get('/rutas/productos_agricolas', (_req, res) => {
+    Rutas.getProductosAgricolas((_err, data) => {
+      res.status(200).json(data)
+    })
+  })
+
+  app.post('/rutas/productos_agricolas', (req, res) => {
+    const userData = {
+      id_producto: null,
+      producto: req.body.producto,
+      tipo_riego: req.body.tipo_riego,
+      tipo_terreno: req.body.tipo_terreno,
+      id_familia: req.body.id_familia,
+      cantidad_hectareas: req.body.cantidad_hectareas
+    }
+    Rutas.postProductosAgricolas(userData, (_err, data) => {
+      if (data && data.insertId) {
+        console.log(data)
+        res.json({
+          success: true,
+          msg: 'Agricola Insertada',
+          data: data
+        })
+      } else {
+        res.status(500).json({
+          success: false,
+          msg: 'error'
+        })
+      }
+    })
+  })
+
+  app.put('/rutas/productos_agricolas/:id_productos_agricolas', (req, res) => {
+    const userData = {
+      id_productos_agricolas: req.params.id_productos_agricolas,
+      producto: req.body.producto,
+      tipo_riego: req.body.tipo_riego,
+      tipo_terreno: req.body.tipo_terreno,
+      id_familia: req.body.id_familia,
+      cantidad_hectareas: req.body.cantidad_hectareas
+    }
+    Rutas.updateColonias(userData, (_err, data) => {
+      if (data && data.msg) {
+        res.json({ data })
+      } else {
+        res.status(500).json({
+          success: false,
+          msg: 'Error'
+        })
+      }
+    })
+  })
+
+  app.delete('/rutas/productos_agricolas/:id', (req, res) => {
+    Rutas.deleteProductoAgricola(req.params.id, (_err, data) => {
+      if ((data && data.msg) === 'Deleted' || data.msg === 'not Exists') {
+        res.json({
+          success: 'true',
+          data
+        })
+      } else {
+        res.status(500).json({
+          msg: 'Error'
+        })
+      }
     })
   })
 }
