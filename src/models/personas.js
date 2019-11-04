f567bbefbaf150eff183df6d9a656e49fcc8bfe2
Julián Barragán Bravo <julidj505@gmail.com>
2019-11-04 15:26:22 -0600

    Personas

--- src/models/personas.js
+++ src/models/personas.js
@@ -5,7 +5,7 @@ let personasModel = {}
 // CONSULTA PERSONAS EN GENERAL
 personasModel.getUser = (callback) => {
   if (connection) {
-    connection.query('SELECT * FROM personas WHERE estatus = 1 ORDER BY id_persona DESC',
+    connection.query('SELECT * FROM persona WHERE estatus = 1 ORDER BY id_persona DESC',
       (err, rows) => {
         if (err) {
           throw err
@@ -19,7 +19,7 @@ personasModel.getUser = (callback) => {
 
 personasModel.getPersonasById = (userId, callback) => {
   if (connection) {
-    connection.query('SELECT * FROM personas WHERE `estatus` = 1 AND `id_persona` = ? ORDER BY id_persona DESC',
+    connection.query('SELECT * FROM persona WHERE `estatus` = 1 AND `id_persona` = ? ORDER BY id_persona DESC',
       (userId), (err, rows) => {
         if (err) {
           throw err
@@ -33,7 +33,7 @@ personasModel.getPersonasById = (userId, callback) => {
 
 personasModel.getUserInner = (callback) => {
   if (connection) {
-    connection.query('SELECT personas.id_persona, personas.primer_nombre, personas.segundo_nombre, personas.a_paterno, personas.a_materno, personas.sexo, personas.fecha_nacimiento, grado_estudios.estudios, personas.escuela, ocupacion.ocupacion, estado_civil.estado, personas.lugar_origen, personas.telefono, familias.nombre_familia, colonias.colonia, familias.direccion, familias.calle_lateral_derecha, familias.calle_lateral_izquierda FROM personas INNER JOIN familias ON id_persona = id_persona INNER JOIN colonias ON familias.id_colonia = colonias.id_colonia INNER JOIN grado_estudios ON personas.id_grado_estudios = grado_estudios.id_grado_estudios INNER JOIN ocupacion ON personas.id_ocupacion = ocupacion.id_ocupacion INNER JOIN estado_civil ON personas.id_estado_civil = estado_civil.id_estado_civil WHERE personas.estatus = 1 ORDER BY id_persona DESC;',
+    connection.query('SELECT persona.id_persona, personas.primer_nombre, personas.segundo_nombre, personas.a_paterno, personas.a_materno, personas.sexo, personas.fecha_nacimiento, grado_estudios.estudios, personas.escuela, ocupacion.ocupacion, estado_civil.estado, personas.lugar_origen, personas.telefono, familias.nombre_familia, colonias.colonia, familias.direccion, familias.calle_lateral_derecha, familias.calle_lateral_izquierda FROM personas INNER JOIN familias ON id_persona = id_persona INNER JOIN colonias ON familias.id_colonia = colonias.id_colonia INNER JOIN grado_estudios ON personas.id_grado_estudios = grado_estudios.id_grado_estudios INNER JOIN ocupacion ON personas.id_ocupacion = ocupacion.id_ocupacion INNER JOIN estado_civil ON personas.id_estado_civil = estado_civil.id_estado_civil WHERE personas.estatus = 1 ORDER BY id_persona DESC;',
       (err, rows) => {
         if (err) {
           throw err
@@ -47,7 +47,7 @@ personasModel.getUserInner = (callback) => {
 
 personasModel.insertPersona = (userData, callback) => {
   if (connection) {
-    connection.query('INSERT INTO personas SET ?', userData,
+    connection.query('INSERT INTO persona SET ?', userData,
       (err, result) => {
         if (err) {
           throw err
@@ -64,7 +64,7 @@ personasModel.insertPersona = (userData, callback) => {
 personasModel.updateUser = (userData, callback) => {
   if (connection) {
     const sql = `
-      UPDATE personas SET 
+      UPDATE persona SET 
       primer_nombre = ${connection.escape(userData.primer_nombre)},
       segundo_nombre = ${connection.escape(userData.segundo_nombre)},
       a_paterno = ${connection.escape(userData.a_paterno)},
@@ -95,10 +95,10 @@ personasModel.updateUser = (userData, callback) => {
 personasModel.deleteUser = (id, callback) => {
   if (connection) {
     let sqlExists =
-      `SELECT * FROM personas WHERE id_persona = ${connection.escape(id)}`
+      `SELECT * FROM persona WHERE id_persona = ${connection.escape(id)}`
     connection.query(sqlExists, (_err, row) => {
       if (row) {
-        let sql = `UPDATE personas SET estatus = 0 WHERE id_persona = ${connection.escape(id)}`
+        let sql = `UPDATE persona SET estatus = 0 WHERE id_persona = ${connection.escape(id)}`
         connection.query(sql, (err, _result) => {
           if (err) {
             throw err
